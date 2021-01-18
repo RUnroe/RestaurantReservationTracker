42a979a8a9bdcf5efb2a7334a1a72ac360d8aac2
RUnroe <ryanunroe@gmail.com>
2021-01-18 13:19:06 -0700

    Create reservation model

--- RestaurantReservationTracker/Models/Reservation.cs
+++ RestaurantReservationTracker/Models/Reservation.cs
@@ -7,5 +7,37 @@ namespace RestaurantReservationTracker.Models
 {
     public class Reservation
     {
+        public int ID { get; set; }
+        public string DateTime { get; set; } = "1970-1-1T00:00";
+        public string Name { get; set; } = "[NO NAME]";
+        public int Size { get; set; } = 0;
+        public bool InSmokingSection { get; set; } = false;
+        public string SpecialInstructions { get; set; } = "";
+        public string ContactNumber { get; set; } = "[NO NUMBER]";
+        public bool CanText { get; set; } = false;
+
+
+        public Reservation() { }
+
+        public Reservation(int id, string dateTime, string name, int size, bool inSmokingSection, string specialInstructions, string contactNumber, bool canText)
+        {
+            this.ID = id;
+            this.DateTime = dateTime;
+            this.Name = name;
+            this.Size = size;
+            this.InSmokingSection = inSmokingSection;
+            this.SpecialInstructions = specialInstructions;
+            this.ContactNumber = contactNumber;
+            this.CanText = canText;
+
+        }
+
+
+        public override string ToString()
+        {
+            string date = $"{this.DateTime.Split('-')[1]}/{this.DateTime.Split('-')[2]}" ;
+            string time = this.DateTime.Split("T")[1];
+            return $"{time} {date} name"; 
+        }
     }
 }
